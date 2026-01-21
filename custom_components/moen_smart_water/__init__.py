182a69eddd9a7f33228d96514a5c344ffd9a8dd3
Alexander Bell-Towne <alexander@bell-towne.com>
2026-01-20 22:04:09 -0800

    Bump version to 0.9.7

--- custom_components/moen_smart_water/__init__.py
+++ custom_components/moen_smart_water/__init__.py
@@ -6,7 +6,7 @@ import logging
 import time
 from typing import Any
 
-__version__ = "0.9.6"
+__version__ = "0.9.7"
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
